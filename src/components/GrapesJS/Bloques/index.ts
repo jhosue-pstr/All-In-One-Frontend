7a243b492b5f1238e30fe19d581ad2242e4b1166
jhosue-pstr <pastor.jhosue07@gmail.com>
2026-04-25 18:31:42 -0500

    arreglando sonarcloud manteability 4

--- src/components/GrapesJS/Bloques/index.ts
+++ src/components/GrapesJS/Bloques/index.ts
@@ -1,14 +1,74 @@
-export { BloqueTitulo, BloqueTexto, BloqueHeading, BloqueImagen, BloqueBoton, BloqueLink, BloqueSeccion, BloqueSpacer, BloqueLista, BloqueQuote, BloqueDivider, BloqueBadge, BloqueIcono, BloqueVideo as BasicoVideo, BloqueAudio as BasicoAudio, BloqueMapa as BasicoMapa } from "./Basico";
+import * as Basico from "./Basico";
+import * as Multimedia from "./Multimedia";
+import * as Seccion from "./Seccion";
+import * as Layout from "./Layout";
+import * as SiteAuth from "./SiteAuth";
 
-export { BloqueImagen as MultimediaImagen, BloqueImagenMediana, BloqueImagenChica, BloqueImagenPequena, BloqueGaleria, BloqueVideo as MultimediaVideo, BloqueVideoMP4, BloqueAudio as MultimediaAudio, BloqueMapa as MultimediaMapa, BloqueMapaInteractivo } from "./Multimedia";
+export const BloqueTitulo = Basico.BloqueTitulo;
+export const BloqueTexto = Basico.BloqueTexto;
+export const BloqueHeading = Basico.BloqueHeading;
+export const BloqueImagen = Basico.BloqueImagen;
+export const BloqueBoton = Basico.BloqueBoton;
+export const BloqueLink = Basico.BloqueLink;
+export const BloqueSeccion = Basico.BloqueSeccion;
+export const BloqueSpacer = Basico.BloqueSpacer;
+export const BloqueLista = Basico.BloqueLista;
+export const BloqueQuote = Basico.BloqueQuote;
+export const BloqueDivider = Basico.BloqueDivider;
+export const BloqueBadge = Basico.BloqueBadge;
+export const BloqueIcono = Basico.BloqueIcono;
+export const BasicoVideo = Basico.BloqueVideo;
+export const BasicoAudio = Basico.BloqueAudio;
+export const BasicoMapa = Basico.BloqueMapa;
 
-export { BloqueSeccion as SeccionBloque, BloqueHeader, BloqueNavbar, BloqueHero, BloqueHeroGrande, BloqueServicios, BloqueFeatures, BloqueNosotros, BloqueEquipo, BloqueCTA, BloqueTestimonios, BloquePrecios, BloqueFAQ, BloqueBlogGrid, BloqueContacto, BloqueFooter } from "./Seccion";
+export const MultimediaImagen = Multimedia.BloqueImagen;
+export const BloqueImagenMediana = Multimedia.BloqueImagenMediana;
+export const BloqueImagenChica = Multimedia.BloqueImagenChica;
+export const BloqueImagenPequena = Multimedia.BloqueImagenPequena;
+export const BloqueGaleria = Multimedia.BloqueGaleria;
+export const MultimediaVideo = Multimedia.BloqueVideo;
+export const BloqueVideoMP4 = Multimedia.BloqueVideoMP4;
+export const MultimediaAudio = Multimedia.BloqueAudio;
+export const MultimediaMapa = Multimedia.BloqueMapa;
+export const BloqueMapaInteractivo = Multimedia.BloqueMapaInteractivo;
 
-export { BloqueGrid2, BloqueGrid3, BloqueGrid4, BloqueContenedor, BloqueSidebarIzquierdo, BloqueSidebarDerecho, BloqueBlogLayout, BloqueCardGrid, BloqueSplitScreen } from "./Layout";
+export const SeccionBloque = Seccion.BloqueSeccion;
+export const BloqueHeader = Seccion.BloqueHeader;
+export const BloqueNavbar = Seccion.BloqueNavbar;
+export const BloqueHero = Seccion.BloqueHero;
+export const BloqueHeroGrande = Seccion.BloqueHeroGrande;
+export const BloqueServicios = Seccion.BloqueServicios;
+export const BloqueFeatures = Seccion.BloqueFeatures;
+export const BloqueNosotros = Seccion.BloqueNosotros;
+export const BloqueEquipo = Seccion.BloqueEquipo;
+export const BloqueCTA = Seccion.BloqueCTA;
+export const BloqueTestimonios = Seccion.BloqueTestimonios;
+export const BloquePrecios = Seccion.BloquePrecios;
+export const BloqueFAQ = Seccion.BloqueFAQ;
+export const BloqueBlogGrid = Seccion.BloqueBlogGrid;
+export const BloqueContacto = Seccion.BloqueContacto;
+export const BloqueFooter = Seccion.BloqueFooter;
 
-export { BloqueLoginSimple, BloqueLoginCard, BloqueLoginMinimal, BloqueRegistroSimple, BloqueRegistroCard, BloqueRegistroMinimal, BloqueLoginGoogle, BloqueLoginSocial, BloquePerfilSimple, BloquePerfilDetallado } from "./SiteAuth";
+export const BloqueGrid2 = Layout.BloqueGrid2;
+export const BloqueGrid3 = Layout.BloqueGrid3;
+export const BloqueGrid4 = Layout.BloqueGrid4;
+export const BloqueContenedor = Layout.BloqueContenedor;
+export const BloqueSidebarIzquierdo = Layout.BloqueSidebarIzquierdo;
+export const BloqueSidebarDerecho = Layout.BloqueSidebarDerecho;
+export const BloqueBlogLayout = Layout.BloqueBlogLayout;
+export const BloqueCardGrid = Layout.BloqueCardGrid;
+export const BloqueSplitScreen = Layout.BloqueSplitScreen;
 
-import { BloqueTitulo, BloqueTexto, BloqueHeading, BloqueImagen, BloqueBoton, BloqueLink, BloqueSeccion, BloqueSpacer, BloqueLista, BloqueQuote, BloqueDivider, BloqueBadge, BloqueIcono, BloqueVideo as BasicoVideo, BloqueAudio as BasicoAudio, BloqueMapa as BasicoMapa, BloqueImagen as MultimediaImagen, BloqueImagenMediana, BloqueImagenChica, BloqueImagenPequena, BloqueGaleria, BloqueVideo as MultimediaVideo, BloqueVideoMP4, BloqueAudio as MultimediaAudio, BloqueMapa as MultimediaMapa, BloqueMapaInteractivo, BloqueHeader, BloqueNavbar, BloqueHero, BloqueHeroGrande, BloqueServicios, BloqueFeatures, BloqueNosotros, BloqueEquipo, BloqueCTA, BloqueTestimonios, BloquePrecios, BloqueFAQ, BloqueBlogGrid, BloqueContacto, BloqueFooter, BloqueGrid2, BloqueGrid3, BloqueGrid4, BloqueContenedor, BloqueSidebarIzquierdo, BloqueSidebarDerecho, BloqueBlogLayout, BloqueCardGrid, BloqueSplitScreen, BloqueLoginSimple, BloqueLoginCard, BloqueLoginMinimal, BloqueRegistroSimple, BloqueRegistroCard, BloqueRegistroMinimal, BloqueLoginGoogle, BloqueLoginSocial, BloquePerfilSimple, BloquePerfilDetallado } from "./index";
+export const BloqueLoginSimple = SiteAuth.BloqueLoginSimple;
+export const BloqueLoginCard = SiteAuth.BloqueLoginCard;
+export const BloqueLoginMinimal = SiteAuth.BloqueLoginMinimal;
+export const BloqueRegistroSimple = SiteAuth.BloqueRegistroSimple;
+export const BloqueRegistroCard = SiteAuth.BloqueRegistroCard;
+export const BloqueRegistroMinimal = SiteAuth.BloqueRegistroMinimal;
+export const BloqueLoginGoogle = SiteAuth.BloqueLoginGoogle;
+export const BloqueLoginSocial = SiteAuth.BloqueLoginSocial;
+export const BloquePerfilSimple = SiteAuth.BloquePerfilSimple;
+export const BloquePerfilDetallado = SiteAuth.BloquePerfilDetallado;
 
 export const blocks = [
   BloqueTitulo,
