102738b002dfb75d3d9e776cff60f626ac50ca32
jhosue-pstr <pastor.jhosue07@gmail.com>
2026-06-04 07:49:06 -0500

    agregando bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
 
-// Verificación explícita para evitar el operador "!" y complacer a SonarQube
 if (!rootElement) {
-  /* v8 ignore next 3 */
   throw new Error("No se encontró el elemento root en el HTML.");
 }
 
